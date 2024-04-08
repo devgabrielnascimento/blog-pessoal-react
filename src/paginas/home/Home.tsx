abf93c885a1cd9c00d61f2cbe7dd7b284e50da0c
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-04-08 19:20:01 -0300

    first commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+function Home(props) {
+  return (
+<>
+<header>
+  início
+</header>
+      <h1 className='titulo'>Home</h1>
+      <img src="https://th.bing.com/th/id/OIG3.oDSd10JHwucf.NZziaM.?pid=ImgGn" alt="paisagem" className='img' />
+      </>
+    );
+    
+}
+
+export default Home;
