79bc91e54670063be72a0852376865ddfed32119
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-04-10 18:05:30 -0300

    adicionando routas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import Home from './paginas/home/Home';
 import './App.css';
-
+import Home from './paginas/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-   <Home taskTitle="Task: " 
-   taskDescription="Finish canvas activities:" 
-   taskID= {1} />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
