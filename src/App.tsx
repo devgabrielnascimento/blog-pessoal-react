83baf15d7b0b2970a9558bc818e5954a5960c081
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-04-11 20:30:15 -0300

    começando página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
+import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+   
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+  
     </>
-);
+  );
 }
 export default App;
