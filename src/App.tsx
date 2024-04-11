21cd40e9dc4d3ef966d41cb34313c94a28b402fc
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-04-11 20:58:19 -0300

    página de cadastro e login

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 function App() {
   return (
     <>
-   
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
@@ -25,7 +26,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-  
+        </AuthProvider>
     </>
   );
 }
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-eurj.onrender.com'
+  baseURL: 'https://blogpessoal-eurj.onrender.com/'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
@@ -12,23 +12,4 @@ export const cadastrarUsuario = async(url: string, dados: Object, setDados: Func
 export const login = async(url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
-}
-
-export const buscar = async(url: string, setDados: Function, header: Object) => {
-  const resposta = await api.get(url, header)
-  setDados(resposta.data)
-}
-
-export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-  const resposta = await api.post(url, dados, header)
-  setDados(resposta.data)
-}
-
-export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-  const resposta = await api.put(url, dados, header)
-  setDados(resposta.data)
-}
-
-export const deletar = async(url: string, header: Object) => {
-  await api.delete(url, header)
 }
