9c9b68c95944e8040c939305cb50b1237794c1e5
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-04-19 15:57:22 -0300

    ....

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://blogpessoal-eurj.onrender.com"
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
